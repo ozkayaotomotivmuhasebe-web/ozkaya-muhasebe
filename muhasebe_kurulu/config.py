b7be4c7202c4dc48c03cf737e6b4eb69da0c3890
OZKAYA <kolayinsanlar@gmail.com>
2026-03-03 13:37:37 +0300

    fix: versiyon 1.0.9 - guncelleme bildirimi duzeltildi

--- muhasebe_kurulu/config.py
+++ muhasebe_kurulu/config.py
@@ -14,7 +14,7 @@ DATABASE_URL = f"sqlite:///{DATABASE_DIR / 'muhasebe.db'}"
 
 # Uygulama ayarları
 APP_NAME = "Muhasebe Takip Sistemi"
-APP_VERSION = "1.0.8"
+APP_VERSION = "1.0.9"
 WINDOW_WIDTH = 1400
 WINDOW_HEIGHT = 900
 
